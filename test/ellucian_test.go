eead8f774bf445d631bc8c3f3ca1101698858da6
Alec Rabold <alecrabold@outlook.com>
2021-12-12 19:30:26 -0800

    add e2e test suite

---
+++ test/ellucian_test.go
@@ -0,0 +1,78 @@
+package test
+
+import (
+	"math/rand"
+
+	"github.com/alec-rabold/course-data-api/pkg/model/request"
+	"github.com/alec-rabold/course-data-api/pkg/util/ellucian"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ellucian client", func() {
+	It("should successfully implement GetColleges()", func() {
+		By("executing GetColleges()")
+		Expect(ellucianClient.GetColleges(ctx)).ToNot(BeEmpty())
+	})
+
+	// NOTE: as this is not meant to be an exhaustive suite,
+	// the tested term, subject, and course are chosen at random
+	Context("for each supported college", func() {
+		DescribeTable("implements API operations", func(college ellucian.College) {
+			By("executing GetTerms()")
+			terms, err := ellucianClient.GetTerms(ctx, request.Terms{
+				College: string(college),
+			})
+			Expect(err).To(BeNil())
+			Expect(terms).ToNot(BeEmpty())
+
+			By("executing GetSubjects()")
+			term := terms[rand.Intn(len(terms))]
+			GinkgoWriter.Write([]byte(term.TermCode))
+			subjects, err := ellucianClient.GetSubjects(ctx, request.Subjects{
+				College: string(college),
+				Term:    term.TermCode,
+			})
+			Expect(err).To(BeNil())
+			Expect(subjects).ToNot(BeEmpty())
+
+			By("executing GetCourses()")
+			subject := subjects[rand.Intn(len(subjects))]
+			GinkgoWriter.Write([]byte(subject.Abbreviation))
+			courses, err := ellucianClient.GetCourses(ctx, request.Courses{
+				College: string(college),
+				Term:    term.TermCode,
+				Subject: subject.Abbreviation,
+			})
+			Expect(err).To(BeNil())
+			Expect(courses).ToNot(BeEmpty())
+
+			By("executing GetSections()")
+			course := courses[rand.Intn(len(courses))]
+			GinkgoWriter.Write([]byte(course.CourseNumber))
+			sections, err := ellucianClient.GetSections(ctx, request.Sections{
+				College: string(college),
+				Term:    term.TermCode,
+				Subject: subject.Abbreviation,
+				Number:  course.CourseNumber,
+			})
+			Expect(err).To(BeNil())
+			Expect(sections).ToNot(BeEmpty())
+		},
+			entries()...,
+		)
+	})
+})
+
+func entries() []TableEntry {
+	var entries []TableEntry
+	for college, name := range ellucian.SupportedColleges {
+		entries = append(entries, Entry(name, college))
+		// TEMP
+		break
+		// TEMP
+	}
+	return entries
+}
---
+++ test/suite_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/alec-rabold/course-data-api/pkg/client/ellucian"
+	"github.com/alec-rabold/course-data-api/pkg/crawler"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var ctx context.Context
+
+// Shared pool of supported client
+var (
+	ellucianClient *ellucian.Client
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "E2E Suite Test")
+}
+
+var _ = BeforeSuite(func() {
+	ctx = context.Background()
+	ellucianClient = ellucian.NewClient(crawler.NewCrawler(
+		crawler.WithContentType("application/x-www-form-urlencoded"),
+	))
+})
